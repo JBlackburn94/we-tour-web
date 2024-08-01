f0d958a1292eaa2a929ab964dfe98e7088fb10df
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-08-01 16:51:59 +0100

    inital commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "#212A33",
+        cardBackground: "#1E2730",
+        cardBackgroundTint: "#39404C",
+        accentOrange: "#FE5659",
+        orangeTint: "#FF998B",
       },
     },
   },
